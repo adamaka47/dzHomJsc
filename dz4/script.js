6a710e3f20957cbfae56d171277a1273ee31dfdc
Adam <lol11211223@yandex.ru>
2019-06-21 22:35:44 +0400

    Четвертое домашнее задание выполнено!

--- dz4/script.js
+++ dz4/script.js
@@ -1,74 +1,97 @@
-// let
-// 	money,
-// 	name,
-// 	time;
+let
+	money,
+	name,
+	time,
+	price;
 
-// function start(discount) {
-// 	money = prompt('Сколько денег имеется?');
-// 	console.log(money);
-// 	if (discount) {
-// 		money *= 0.8;
-// 		console.log(money);
-// 	}
+function start() {
+	money = prompt('Ваш бюджет?', '');
+	while (isNaN(money) || money == '' || money == null) {
+		money = prompt('Ваш бюджет?', '');
+	}
+	name = prompt('Название вашего магазина?', '').toUpperCase();
+	time = 24
+}
 
-// 	while (isNaN(money) || money == '' || money == null) {
-// 		money = prompt('Сколько денег имеется?');
-// 		if (discount) {
-// 			money *= 0.8;
-// 			console.log(money);
-// 		}
-// 	}
+// start();
 
-// 	name = prompt('Введите название магазина');
-// 	time = 24;
-// }
+var myList = {
+	budget: money,
+	shopName: name,
+	employers: {},
+	shopGoods: [],
+	open: false,
+	discount: false,
+	shopItems: [],
+	choose: function choose() {
+		for (let i = 0; i < 5; i++) {
+			let ans = prompt('Какой тип товаров будем продавать?');
+			if ((typeof(ans)) === 'string' && (typeof(ans)) != null && ans != '' && ans.length < 50) {
+				myList.shopGoods[i] = ans
+			} else {
+				i = i - 1
+			}
+		}
+	},
+	works: function works(time) {
+		if (time < 0) {
+			alert('Ты шутишь)0')
+			} else if (time >= 8 && time < 21) {
+				alert('Проходи братик');
+				myList.open = true;
+				} else if (time < 24) {
+					alert('Поздно(9')
+					} else {
+						alert('Хватит рофлов!')
+						}
+	},
+	days: function days() {
+		alert('Ежедневный бюджет: ' + myList.budget / 30);
+	},
+	discounts: function discounts() {
+		if (myList.discount == true) {
+			price *= 0.8;
+		}
+	},
 
-// start(true);
+	employerses: function employerses() {
+		for (let i = 1; i < 4; i++) {
+			let nameses = prompt('Ваше имя');
+			myList.employers[i] = nameses;
+		}
+	},
+	chooseShop: function chooseShop() {
+		let items = prompt('Перечислите товары через запятую', '');
+		if ((typeof(items)) === 'string' && (typeof(items)) != null && (typeof(items)) != '') {
+			myList.shopItems = items.split(",");
+			myList.shopItems.push(prompt('Подождите еще, ', ''));
+			myList.shopItems.sort();
+			} else {
+				alert('Введите нормальный запрос!')
+				}
+		myList.shopItems.forEach(function(item) {
+			alert('У нас вы можете купить: ' + item);
+		});
 
-// let myList = {
-// 	allMoney: money,
-// 	shopName: name,
-// 	shopGoods: [],
-// 	employers: {},
-// 	open: true,
-// 	discount: false
-// }
-
-// console.log(myList.employers.name);
-
-// function chooses() {
-// 	for (let i = 0; i < 5; i++) {
-// 		let ans = prompt('Каков тип товара?');
-// 		if ((typeof(ans)) === "string" && (typeof(ans)) != null && ans != '' && ans.length < 50) {
-// 			console.log('Успешно');
-// 			myList.shopGoods[i] = ans;
-// 		} else {
-// 			console.log('Безуспешно!');
-// 		}
-// 	}
-// }
-
-// chooses();
+		for (let i in myList.shopItems) {
+			alert('Наш магазин включает в себя: ' +myList.shopItems[i]);
+		}
 
 
-// function works(time) {
-// 	if (time < 0) {
-// 		console.log('Невозможно');
-// 		} else if (time >= 8 && time < 20) {
-// 			console.log('Должно работать...')
-// 			} else if (time < 24) {
-// 				console.log('Вы опоздали...')
-// 				} else {
-// 					console.log('Это нереально(')
-// 					}
-// }
+	},
 
-// works(9);
+}
+
+myList.chooseShop();
+
+// 
+// choose();
 
 
-// console.log(myList.shopGoods);
+// works(14);
 
-// alert(myList.allMoney / 30);
 
-// console.log(myList);
 
+// employerses();
+
+console.log(myList);
